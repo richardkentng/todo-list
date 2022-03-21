3ba87ae7666b3111194d0523f8232b274bd40699
Richard Ng <richardkentng@gmail.com>
2022-03-20 18:54:39 -0700

    adds add-todo functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 console.log("sanity check");
+
+const todoForm = document.body.querySelector(".todo-form");
+const todoInput = todoForm.querySelector("#todo-input");
+const todoList = document.body.querySelector(".todo-list");
+
+todoForm.addEventListener("submit", onSubmitTodoForm);
+
+function onSubmitTodoForm(e) {
+  e.preventDefault();
+
+  const todoLi = document.createElement("li");
+  todoLi.textContent = todoInput.value;
+  todoList.appendChild(todoLi);
+}
