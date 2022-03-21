ba2fbc4cb49c5efd606915c05f8449b08ba83346
Richard Ng <richardkentng@gmail.com>
2022-03-20 19:17:34 -0700

    saves todos to local storage

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,43 @@
 console.log("sanity check");
 
+displayTodos(getTodos());
+
 const todoForm = document.body.querySelector(".todo-form");
 const todoInput = todoForm.querySelector("#todo-input");
-const todoList = document.body.querySelector(".todo-list");
 
 todoForm.addEventListener("submit", onSubmitTodoForm);
+
+//=================================================/
+//-------------------FUNCTIONS --------------------/
+//=================================================/
 
 function onSubmitTodoForm(e) {
   e.preventDefault();
 
-  const todoLi = document.createElement("li");
-  todoLi.textContent = todoInput.value;
-  todoList.appendChild(todoLi);
+  //create todo object
+  const newTodo = {
+    text: todoInput.value,
+    done: false,
+    doneTime: "",
+    createdTime: Date.now(),
+  };
+
+  // add one todo to local storage
+  const savedTodos = getTodos();
+  savedTodos.push(newTodo);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+  displayTodos(savedTodos);
+}
+
+function displayTodos(todoObjs) {
+  //map through todo objects to create HTML
+  const todosStr = todoObjs.map((todo) => `<li>${todo.text}</li>`).join("");
+  //display todos
+  const todoList = document.body.querySelector(".todo-list");
+  todoList.innerHTML = todosStr;
+}
+
+function getTodos() {
+  return JSON.parse(localStorage.getItem("todos")) || [];
 }
